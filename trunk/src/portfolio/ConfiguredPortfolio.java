d8d622e784e0dcc9f4fb570f5123868208dd43ec
mauro.vallati@gmail.com <mauro.vallati@gmail.com@86b76ba5-9f2a-addc-694c-d072e0f91f87>
2013-02-25 09:40:41 +0000

    Finally, added the missed class ;)

---
+++ trunk/src/portfolio/ConfiguredPortfolio.java
@@ -0,0 +1,71 @@
+package portfolio;
+
+import java.util.Arrays;
+import java.util.Vector;
+
+public class ConfiguredPortfolio {
+    //THE configured portfolio. OrderedPlanners is a list of ids of selected planners,
+	// CPU_time is a list of the CPU_time that the planner will run.
+
+	int[] orderedPlanners;
+	Vector <Vector<Float> > CPU_time;
+	double score; // the score achieved by that portfolio on the training problems
+	
+	public double getScore() {
+		return score;
+	}
+
+	public void setScore(double score) {
+		this.score = score;
+	}
+
+	public ConfiguredPortfolio(int size){
+		CPU_time = new Vector <Vector<Float>>();
+		orderedPlanners = new int[size];
+		for(int i=0; i < size; i++){
+			orderedPlanners[i]=-1; //-1 means not allocated
+			Vector<Float> fake = new Vector<Float>();
+			CPU_time.add(fake);
+			score=0.0;
+		}
+	}
+	
+	public ConfiguredPortfolio() {
+		orderedPlanners[0]=-1;
+		CPU_time = new Vector <Vector<Float>>();
+	}
+	
+	public int[] getOrderedPlanners() {
+		return orderedPlanners;
+	}
+
+	public void setOrderedPlanners(int[] orderedPlanners) {
+		this.orderedPlanners = orderedPlanners;
+	}
+
+	public void addPlanner(int id){
+		int i=0;
+		while(orderedPlanners[i]!=-1)
+			i++;
+		orderedPlanners[i]=id;
+		return;
+	}
+	
+	public Vector<Vector<Float>> getCPU_time() {
+		return CPU_time;
+	}
+
+	public void setCPU_time(Vector<Vector<Float>> cPU_time) {
+		CPU_time = cPU_time;
+	}
+
+	@Override
+	public String toString() {
+		return "ConfiguredPortfolio [orderedPlanners="
+				+ Arrays.toString(orderedPlanners) + ", CPU_time=" + CPU_time
+				+ ", score=" + score + "]";
+	}
+
+
+
+}
