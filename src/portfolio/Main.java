af0764a93c5198bf53c44a3a9d2d7c9a487f45de
mauro.vallati <mauro.vallati@gmail.com>
2013-04-12 17:32:06 +0000

    Added some documentation in java code

--- src/portfolio/Main.java
+++ src/portfolio/Main.java
@@ -11,10 +11,15 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		String file="input/input.xml";
+		// reading the input file
 		portfolio_configuration= new Configuration(file);
+		// printing out the information read in input
 		portfolio_configuration.print_everything();
+		// solving the training problems
 		portfolio_configuration.solve_problems();
+		//configuring the portfolio, following the structure given in input
 		portfolio_configuration.configure();
+		//saving the information received in input, not the configured portfolio
 		portfolio_configuration.save("input/output.xml");
 	}
 
